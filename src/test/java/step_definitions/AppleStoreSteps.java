423e7f72ba83413be3aebc5fefe2812a26896543
nibirH54 <nibirw@gmail.com>
2021-09-23 19:04:30 -0400

    Initial commit

---
+++ src/test/java/step_definitions/AppleStoreSteps.java
@@ -0,0 +1,50 @@
+package step_definitions;
+
+import command_providers.ActOn;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AppleStoreSteps {
+
+    private static final By StoreButton = By.xpath("//*[@id='ac-globalnav']/div/ul[2]/li[2]/a");
+    private static final By ClickMacLink = By.xpath("//*[@id='shelf-1_section']/div/div[1]/div/div/div[1]/div/div/div/div[2]/div/a");
+
+    WebDriver driver = Hooks.driver;
+    private static final Logger LOGGER = LogManager.getLogger(AppleStoreSteps.class);
+
+
+    @Given("^a user is at apple page$")
+    public void userLandsAtApplePage() {
+        ActOn.browser(driver).openBrowser("https://www.apple.com/");
+        LOGGER.info("User is on Apple.com home page");
+
+    }
+
+    @When("^user clicks on store option in menu$")
+    public void clickOnStoreOption() {
+        ActOn.element(driver, StoreButton).click();
+        LOGGER.info("User clicks on store option in menu");
+
+    }
+    @Then("^user lands on retail page$")
+    public void landsOnRetailPage(){
+        LOGGER.info("User is at the retail page");
+
+    }
+
+    @And("^user clicks on Mac option to navigate to the mac page$")
+    public void clickOnFindNewDeals() {
+        ActOn.element(driver, ClickMacLink).click();
+        LOGGER.info("User clicks on Mac option to navigate to Mac page");
+
+    }
+
+
+    }
+
